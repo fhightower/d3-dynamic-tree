cf89f2163f0deb62709cec976f049b258c5cca01
Floyd Hightower <floyd.hightower27@gmail.com>
2017-02-10 09:31:32 -0500

    Added license headers to the js files

--- js/d3Utility.js
+++ js/d3Utility.js
@@ -1,3 +1,19 @@
+// Modification of the base, D3 collapsible tree code found here: https://bl.ocks.org/mbostock/4339083
+// Copyright (C) 2017  Floyd Hightower
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 var D3UTILITY = D3UTILITY || {};
 
 var tree, root, svg, i, diagonal, duration;
